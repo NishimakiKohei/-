280dd5b91271bd7270e34ab2a4ced898e3294656
U-PS14A14\vantan <deadblue0518@yahoo.co.jp>
2014-11-12 20:06:55 +0900

    Ｔｅｘｔｕｒｅが…

--- ChallengeBasic8/src/main.cpp
+++ ChallengeBasic8/src/main.cpp
@@ -23,43 +23,79 @@ const int ENEMY_NUM = 10;
 
 int pos_x, pos_y;
 int touch;
+int count=0;
+int flag = 0;
 
 Object enemy[ENEMY_NUM];
 
-int hit(Vec2f pos, Vec2f size,Vec2f box_pos,AppEnv  &app_env ){
-	//touchはPushとPressのフラグ用変数
+//int hit(Vec2f pos, Vec2f size,Vec2f box_pos,AppEnv  &app_env ){
+//	//touchはPushとPressのフラグ用変数
+//
+//	/*pos_x = mouse_pos.x();
+//	pos_y = mouse_pos.y();*/
+//
+//	if (pos.x() > box_pos.x()){
+//		if (pos.x() < box_pos.x()+size.x()){
+//			if (pos.y()>box_pos.y()){
+//				if (pos.y() < box_pos.x()+size.y()){
+//
+//					if (app_env.isPushButton(Mouse::LEFT)){
+//						return 1;
+//					}
+//					else if (app_env.isPressButton(Mouse::LEFT)){
+//						return 2;
+//					}
+//				}
+//			}
+//		}
+//	}
+//	return 0;
+//}
 
-	/*pos_x = mouse_pos.x();
-	pos_y = mouse_pos.y();*/
+enum Window {
+	WIDTH = 1320,
+	HEIGHT = 720
+};
 
-	if (pos.x() > box_pos.x()){
-		if (pos.x() < size.x()){
-			if (pos.y()>box_pos.y()){
-				if (pos.y() < size.y()){	
 
-					if (app_env.isPushButton(Mouse::LEFT)){
-						return 1;
-					}
-
-					if (app_env.isPressButton(Mouse::LEFT)){
-						return 2;
-					}
-
-				}
+int hit(Vec2f& point_pos,Vec2f& box_pos,Vec2f& box_size,AppEnv& app_env){
+	if (box_pos.x() < point_pos.x() && point_pos.x() < box_pos.x() + box_size.x()){
+		if (box_pos.y() < point_pos.y() && point_pos.y() < box_pos.y() + box_size.y()){
+			if (app_env.isPushButton(Mouse::LEFT)){
+				return 1;
+			}
+			else if (app_env.isPressButton(Mouse::LEFT)){
+				return 2;
 			}
 		}
 	}
+	return 0;
 }
 
-void Draw(Vec2f box_pos, Vec2f size, int start_tx, int start_ty, Texture &texture){
+
+void Draw(Vec2f box_pos, Vec2f size, int start_tx, int start_ty, Texture& texture){
 	drawTextureBox(box_pos.x(), box_pos.y(), size.x(), size.y(), start_tx, start_ty, size.y(), size.y(), texture, Color(1, 1, 1));
+	//drawBox(box_pos.x(), box_pos.y(), size.x(), size.y(),5,Color(1, 1, 1));
 }
+
+//void test(int a,Random& random){
+//	if (a == 1){
+//		enemy[0].pos.x() = random.fromFirstToLast(-WIDTH / 2, WIDTH / 2);
+//		enemy[0].pos.y() = random.fromFirstToLast(-HEIGHT / 2, HEIGHT / 2);
+//	}
+//	if (a == 2){
+//		for (int i = 0; i < ENEMY_NUM; i++){
+//			enemy[0].pos.x() += random.fromFirstToLast(-1, 1);
+//			enemy[0].pos.y() += random.fromFirstToLast(-1, 1);
+//		}
+//	}
+//}
 
 void Init(){
 	for (int i = 0; i < ENEMY_NUM; i++){
 		enemy[i].hp = 1;
-		enemy[i].pos = Vec2f(0, 0);
-		enemy[i].size = Vec2f(50, 50);
+		enemy[i].pos = Vec2f (0,0);
+		enemy[i].size = Vec2f(64, 64);
 	}
 
 	/*for (auto &Enemy:enemy){
@@ -71,13 +107,10 @@ void Init(){
 
 
 // アプリのウインドウサイズ
-enum Window {
-  WIDTH  = 1320,
-  HEIGHT = 720
-};
+
 
 Texture mushi_graph("res/064742.png");
-
+Texture mushi_blood("res/nc98815.png");
 
 
 
@@ -91,33 +124,45 @@ int main() {
 
   Init();
 
+  Random random;
+  random.setSeed(count);
   while (1) {
     // アプリウインドウが閉じられたらプログラムを終了
     if (!app_env.isOpen()) return 0;
 
-	//Random random;
+
+
+	//touch = 255;
+
+	count += 1;
 
 	Vec2f mouse_pos = app_env.mousePosition();
 
-	touch = 255;
+	//int a = hit(mouse_pos, enemy[0].size, enemy[0].pos, app_env);
 
-	switch (hit(mouse_pos, enemy[0].size, enemy[0].pos, app_env)){
-	case 1:touch = 0;
-	case 2:touch = 100;
+	if (hit(mouse_pos, enemy[0].pos , enemy[0].size, app_env) == 1){
+		for (int i = 0; i < ENEMY_NUM; i++){
+			Draw(enemy[0].pos, enemy[0].size, 0, 0, mushi_blood);
+		}
+		enemy[0].pos.x() = random.fromFirstToLast(-WIDTH / 2, WIDTH / 2);
+		enemy[0].pos.y() = random.fromFirstToLast(-HEIGHT / 2, HEIGHT / 2);
 	}
 
 	
 
+	if (hit(mouse_pos, enemy[0].pos, enemy[0].size, app_env) == 2){
+		for (int i = 0; i < ENEMY_NUM; i++){
+			enemy[0].pos.x() += random.fromFirstToLast(-1, 1);
+			enemy[0].pos.y() += random.fromFirstToLast(-1, 1);
+		}
+	}
     // 描画準備
     app_env.setupDraw();
-
-	//hit(mouse_pos, enemy[0].size, enemy[0].pos, app_env);
 
     //
     // 描画処理はここでおこなう
     // 
-	//drawTextureBox(0, 0, 50, 50, 0, 0,50,50 ,mushi_graph,Color(1,1,1));
-
+	
 	Draw(enemy[0].pos, enemy[0].size, 0, 0, mushi_graph);
 
 	//drawBox(0, 0, 50, 50, 5, color256(touch, touch, touch));
